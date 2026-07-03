419a7623ce4c6e0ea02d670507ac2a6563b245b5
M Defryan <muhammaddefryan@gmail.com>
2026-07-03 15:47:13 +0700

    done

---
+++ src/app/email-verification/[slug]/page.tsx
@@ -0,0 +1,8 @@
+export default function EmailVerificationPage() {
+  return (
+    <>
+      <input type='password' className='border border-gray-300' />
+      <button>Set Password</button>
+    </>
+  );
+}
